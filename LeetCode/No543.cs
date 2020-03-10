03323b4d94ae6b7c308dff5e720c19f17633c521
Tommy Zou <tommyzzj@hotmail.com>
2020-03-10 19:16:45 +0800

    Renew the branch

--- LeetCode/No543.cs
+++ LeetCode/No543.cs
@@ -4,9 +4,21 @@ namespace LeetCode
 {
     class No543
     {
+        int ans;
         public int DiameterOfBinaryTree(TreeNode root)
         {
-            throw new NotImplementedException();
+            ans = 1;
+            depth(root);
+            return ans - 1;
+        }
+
+        private int depth(TreeNode node)
+        {
+            if (node == null) return 0; // 访问到空节点了，返回0
+            int L = depth(node.left); // 左儿子为根的子树的深度
+            int R = depth(node.right); // 右儿子为根的子树的深度
+            ans = Math.Max(ans, L + R + 1); // 计算d_node即L+R+1 并更新ans
+            return Math.Max(L, R) + 1;
         }
     }
 }
