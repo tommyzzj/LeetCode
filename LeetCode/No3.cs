9f75a9d879c78577d8a0899af3fbe3d16df36ce7
zhijie.zou <zhijie.zou@thermofisher.com>
2020-12-24 15:15:18 +0800

    Add longest substring

---
+++ LeetCode/No3.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class No3
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var occ = new HashSet<Char>();
+            int n = s.Length;
+            int rk = -1, ans = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                if (i != 0)
+                {
+                    occ.Remove(s[i - 1]);
+                }
+
+                while (rk + 1 < n && !occ.Contains(s[rk + 1]))
+                {
+                    occ.Add(s[rk + 1]);
+                    ++rk;
+                }
+                ans = Math.Max(ans, rk - i + 1);
+            }
+
+            return ans;
+        }
+    }
+}
