7e4fa903c6e1b2c195a624794eb95134831aea43
Zou Zhijie <zhijie.zou@thermofisher.com>
2021-04-09 09:27:14 +0800

    Add 154

---
+++ LeetCode/No154.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class No154
+    {
+        public int FindMin(int[] nums)
+        {
+            return nums.Min();
+        }
+    }
+}
