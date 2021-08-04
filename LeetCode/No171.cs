11c2ceb8174dbf041295b59c158324a7a96283fb
zhijie.zou <zhijie.zou@thermofisher.com>
2021-08-04 16:51:30 +0800

    Add No171

---
+++ LeetCode/No171.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeetCode
+{
+    public class No171
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            int result = 0;
+            for (int i = 0; i < columnTitle.Length; i++)
+            {
+                int k = columnTitle[i] - 'A' + 1;
+                result += k * (int)Math.Pow(26, columnTitle.Length - i - 1);
+            }
+            return result;
+        }
+    }
+}
